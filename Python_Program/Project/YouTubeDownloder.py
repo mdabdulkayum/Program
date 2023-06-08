ea4f161118a28bec8cb9293b21a07d77b90f1d32
Md Abdul Kayum <100491156+mdabdulkayum@users.noreply.github.com>
2023-06-08 17:13:37 +0600

    Update YouTubeDownloder.py

--- Python_Program/Project/YouTubeDownloder.py
+++ Python_Program/Project/YouTubeDownloder.py
@@ -2,7 +2,7 @@ from pytube import Playlist
 link=input("Enter YouTube Playlist URL: ")
 yt_playlist = Playlist (link)
 for video in yt_playlist.videos:
-    video.streams.get_highest_resolution ().download ("F:\Program\Python\Project\PlayList")
+    video.streams.get_highest_resolution ().download ()
     print("Video Downloaded: ", video.title)
 
 print("\nAll videos are downloaded.")
